779697770153ef2a047cfb823bfd98df45083b88
Diogo Palma <diogopalma@live.com>
2024-04-04 01:11:19 +0000

    resource mobile create

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,9 @@ import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UserSchema } from './users/interfaces/user.schema';
 import { AccountModule } from './account/account.module';
 import { AuthModule } from './auth/auth.module';
+import { MobileModule } from './mobile/mobile.module';
 
 const ENV = process.env.NODE_ENV;
 
@@ -26,12 +26,10 @@ const ENV = process.env.NODE_ENV;
 
       inject: [ConfigService],
     }),
-    MongooseModule.forFeature([
-      { name: 'User', schema: UserSchema },
-    ]),
     AuthModule,
     UsersModule,
     AccountModule,
+    MobileModule,
 
   ],
   controllers: [AppController],
---
+++ src/mobile/mobile.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { MobileService } from './mobile.service';
+import { MobileController } from './mobile.controller';
+import { ClientKafka, ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'mobile-account_producer_client',
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: 'mobile-account_producer',
+            brokers: [process.env.KAFKA_URL],
+          },
+          consumer: {
+            groupId: 'mobile-account-consumer',
+            allowAutoTopicCreation: true
+          }
+        },
+      },
+    ]),
+  ],
+  controllers: [MobileController],
+  providers: [
+    {
+      provide: 'mobile-account_producer',
+      useFactory: (kafkaClient: ClientKafka) => kafkaClient,
+      inject: ['mobile-account_producer_client'],
+    },
+    MobileService
+  ],
+})
+export class MobileModule { }
